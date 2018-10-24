d6a6352f0bd42009479ed9ace75c178c89f02e86
3A532053 <showhk61@gmail.com>
2018-10-24 17:04:44 +0800

    加入錯誤訊息檔案

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/24
+ * Time: 下午 05:01
+ */
+// resources/views/common/errors.blade.php
+
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+
+        <br><br>
+
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
